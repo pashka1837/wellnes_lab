4bf1923326425982dc0909247492880efca2d5f2
pashka1837 <pashka1837@yandex.kz>
2024-05-10 19:30:41 +0500

    feat: router 6.16

--- src/main.tsx
+++ src/main.tsx
@@ -2,43 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {
-	Route, RouterProvider, createBrowserRouter, createRoutesFromElements,
+	 RouterProvider, createBrowserRouter,
 } from 'react-router-dom';
 import Landing from './Pages/Landing.tsx';
 import Hero from './Components/Hero/Hero.tsx';
 import Feautures from './Components/Feautures/Feautures.tsx';
 import Signup from './Components/Signup/Signup.tsx';
 
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/' element={<Landing/>}>
-			<Route index={true} element={<Hero/>}/>
-			<Route path='features' element={<Feautures/>}/>
-			<Route path='signup' element={<Signup/>}/>
-		</Route>,
-	),
-);
+// Const router = createBrowserRouter(
+// 	createRoutesFromElements(
+// 		<Route path='/' element={<Landing/>} >
+// 			<Route index={true} element={<Hero/>}/>
+// 			<Route path='features' element={<Feautures/>}/>
+// 			<Route path='signup' element={<Signup/>}/>
+// 		</Route>,
+// 	),
+// );
 
-// Const router = createBrowserRouter([
-// 	{
-// 		path: '/',
-// 		element: <Landing />,
-// 		children: [
-// 			{
-// 				index: true,
-// 				element: <Hero />,
-// 			},
-// 			{
-// 				path: 'features',
-// 				element: <Feautures />,
-// 			},
-// 			{
-// 				path: 'signup',
-// 				element: <Signup />,
-// 			},
-// 	  ],
-// 	},
-// ]);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Landing />,
+		children: [
+			{
+				index: true,
+				element: <Hero />,
+			},
+			{
+				path: 'features',
+				element: <Feautures />,
+			},
+			{
+				path: 'signup',
+				element: <Signup />,
+			},
+	  ],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
