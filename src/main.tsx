8ac5308b5ec307b42ca05d3859cf814876b154e7
pashka1837 <pashka1837@yandex.kz>
2024-05-10 21:54:25 +0500

    feat: no outlet

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import './index.css';
 import {
 	 RouterProvider, createBrowserRouter,
 } from 'react-router-dom';
-import Landing from './Pages/Landing.tsx';
+// Import Landing from './Pages/Landing.tsx';
 import Hero from './Components/Hero/Hero.tsx';
 import Feautures from './Components/Feautures/Feautures.tsx';
 import Signup from './Components/Signup/Signup.tsx';
+// Import Navbar from './Components/Navbar/Navbar.tsx';
 
 // Const router = createBrowserRouter(
 // 	createRoutesFromElements(
@@ -20,28 +21,32 @@ import Signup from './Components/Signup/Signup.tsx';
 // );
 
 const router = createBrowserRouter([
+	// {
+	// 	path: '/',
+	// 	element: <Landing />,
+	// 	children: [
 	{
 		path: '/',
-		element: <Landing />,
-		children: [
-			{
-				index: true,
-				element: <Hero />,
-			},
-			{
-				path: 'features',
-				element: <Feautures />,
-			},
-			{
-				path: 'signup',
-				element: <Signup />,
-			},
-	  ],
+		element: <Hero />,
 	},
+	{
+		path: 'features',
+		element: <Feautures />,
+	},
+	{
+		path: 'signup',
+		element: <Signup />,
+	},
+	//   ],
+	// },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<>
+			{/* <Navbar /> */}
+			<RouterProvider router={router} />
+		</>
+
 	</React.StrictMode>,
 );
